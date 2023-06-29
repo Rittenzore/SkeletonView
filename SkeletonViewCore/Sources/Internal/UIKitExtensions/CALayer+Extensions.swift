a5135db4a404d3ccc940b0d0061f14929bb97ead
rittenzore <rittenzore@gmail.com>
2023-06-29 09:48:03 +0300

    bugfix: Возвращен вызов на главном потоке

--- SkeletonViewCore/Sources/Internal/UIKitExtensions/CALayer+Extensions.swift
+++ SkeletonViewCore/Sources/Internal/UIKitExtensions/CALayer+Extensions.swift
@@ -53,10 +53,12 @@ extension CALayer {
     
     func playAnimation(_ anim: @escaping SkeletonLayerAnimation, key: String, completion: (() -> Void)? = nil) {
         skeletonSublayers.recursiveSearch(leafBlock: {
-            CATransaction.begin()
-            CATransaction.setCompletionBlock(completion)
-            add(anim(self), forKey: key)
-            CATransaction.commit()
+            DispatchQueue.main.async {
+                CATransaction.begin()
+                CATransaction.setCompletionBlock(completion)
+                self.add(anim(self), forKey: key)
+                CATransaction.commit()
+            }
         }) {
             $0.playAnimation(anim, key: key, completion: completion)
         }
@@ -71,15 +73,17 @@ extension CALayer {
     }
     
     func setOpacity(from: Int, to: Int, duration: TimeInterval, completion: (() -> Void)?) {
-        CATransaction.begin()
-        CATransaction.setCompletionBlock(completion)
-        let animation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
-        animation.fromValue = from
-        animation.toValue = to
-        animation.duration = duration
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-        add(animation, forKey: "setOpacityAnimation")
-        CATransaction.commit()
+        DispatchQueue.main.async {
+            CATransaction.begin()
+            CATransaction.setCompletionBlock(completion)
+            let animation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
+            animation.fromValue = from
+            animation.toValue = to
+            animation.duration = duration
+            animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+            self.add(animation, forKey: "setOpacityAnimation")
+            CATransaction.commit()
+        }
     }
     
     func insertSkeletonLayer(_ sublayer: SkeletonLayer, atIndex index: UInt32, transition: SkeletonTransitionStyle, completion: (() -> Void)? = nil) {
